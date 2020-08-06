0e8b74f22f0eadbebe540bfaff994ca57d5774c7
angela <angelagongli@gmail.com>
2020-08-05 23:12:09 -0500

    Updates

---
+++ precache-manifest.a8a9c5cddb8f3833b7a5e123dcf5ec0c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ef6ebf741f2c33454992c8208a95ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5c3a42596527e8e6d4c",
+    "url": "/static/css/main.6ed14db8.chunk.css"
+  },
+  {
+    "revision": "725780bab27225832fda",
+    "url": "/static/js/2.3a83bb47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a83bb47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5c3a42596527e8e6d4c",
+    "url": "/static/js/main.51fff54e.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "6cb14571a67c8333c1fd551d1ccfdf48",
+    "url": "/static/media/audiophile-screenshot.6cb14571.png"
+  },
+  {
+    "revision": "9dbf7f7fe0e2b287b119833a6600740a",
+    "url": "/static/media/book-search-screenshot.9dbf7f7f.png"
+  },
+  {
+    "revision": "9787126389bf3b0f7d3eb59c72c87fc4",
+    "url": "/static/media/corona-screenshot.97871263.png"
+  },
+  {
+    "revision": "84460ffeff573ad2937911f92b9e3360",
+    "url": "/static/media/door.84460ffe.jpg"
+  },
+  {
+    "revision": "5a69c256f0a91fe373bd2f13135cafbe",
+    "url": "/static/media/dot-grid.5a69c256.png"
+  },
+  {
+    "revision": "10dc1ef43ebbdcf1ab25701ecafc8cea",
+    "url": "/static/media/employee-directory-screenshot.10dc1ef4.png"
+  },
+  {
+    "revision": "ab20fab739eafe4953c0bfc90d6ad636",
+    "url": "/static/media/employee-tracker-demo.ab20fab7.mp4"
+  },
+  {
+    "revision": "f174f06acefb924d229bfa549f403140",
+    "url": "/static/media/employee-tracker-screenshot.f174f06a.png"
+  },
+  {
+    "revision": "7f07f67cc4bce7c0bdb5e74ea3ed431a",
+    "url": "/static/media/gallery.7f07f67c.jpg"
+  },
+  {
+    "revision": "87eef944edfd6e68bf20e80016db587d",
+    "url": "/static/media/headshot-big.87eef944.JPG"
+  },
+  {
+    "revision": "13ee1d8d62b3f5c05b82e3e92bb62b3c",
+    "url": "/static/media/headshot-cropped-big.13ee1d8d.jpg"
+  },
+  {
+    "revision": "3796f6db777714b90637212744ee0168",
+    "url": "/static/media/headshot-cropped.3796f6db.jpg"
+  },
+  {
+    "revision": "dbbb9e5e60c929ec25d24a5d7646e952",
+    "url": "/static/media/headshot-small.dbbb9e5e.JPG"
+  },
+  {
+    "revision": "83f8be52dfed642716a802e77e8ec1c7",
+    "url": "/static/media/joyney-screenshot.83f8be52.png"
+  },
+  {
+    "revision": "2d08157a4221f75804b9999e387d1a4c",
+    "url": "/static/media/mailbox.2d08157a.jpg"
+  },
+  {
+    "revision": "eb4be145cc274a99303fc1500ca868f4",
+    "url": "/static/media/resume.eb4be145.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.db588cee6c2d13d5207825709a95b105.js"
+  "/precache-manifest.a8a9c5cddb8f3833b7a5e123dcf5ec0c.js"
 );
 
 self.addEventListener('message', (event) => {
